b166acb57d84b5be16a695b720f76ce224787f82
Igor.Kornilov <gaber32188@gmail.com>
2021-11-14 13:40:16 +0300

    add simple sort plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,8 @@ module.exports = {
     project: 'tsconfig.json',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  plugins: ['@typescript-eslint/eslint-plugin', 'simple-import-sort'],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   root: true,
   env: {
     node: true,
@@ -20,5 +17,23 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'warn',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'no-duplicate-imports': 'error',
+    // disable standart rules to enable simple-sort-plugins rules
+    'sort-imports': 'off',
+    'import/order': 'off',
+    //@simple-sort-pligins rules
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          ['^@nest', '^@?\\w'],
+          ['vendors', '^\\u0000'],
+          ['types', '^types'],
+          ['exit', '../'],
+          ['deeper', './'],
+        ],
+      },
+    ],
   },
 };
